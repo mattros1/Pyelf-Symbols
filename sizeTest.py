340d75089ae6a2699b3d0c7cd18a5801ecb89271
mattros1 <112406436+mattros1@users.noreply.github.com>
2023-11-25 16:04:08 -0500

    Update sizeTest.py

--- sizeTest.py
+++ sizeTest.py
@@ -1,3 +1,4 @@
+from os import name
 from elftools.elf.elffile import ELFFile
 from elftools.elf.sections import SymbolTableSection
 
@@ -6,45 +7,47 @@ class ELFAnalyzer:
 
   def __init__(self, path):
     self.path = path
-    self.sInfo = {}
 
   def symbols(self):
+    sym_info = {}
     with open(self.path, 'rb') as file:
       elf = ELFFile(file)
-      #iterate through each section
       for section in elf.iter_sections():
         if not isinstance(section, SymbolTableSection):
           continue
-        #iterate through each symbol and 
         for symbol in section.iter_symbols():
-          name = symbol.name
-          addr = symbol['st_value']
-          size = symbol['st_size']
+          if (symbol['st_size'] == 0):
+            continue
+          sym_info[symbol.name] = {
+              'address': symbol['st_value'],
+              'size': symbol['st_size'],
+              'padding': 0
+          }
 
-          self.sInfo[name] = {'address': addr, 'size': size, 'padding': 0}
-          prev = symbol
       #sort by adress to ensure contiguous symbols
-      sortedSymbols = sorted(self.sInfo.items(),
-                             key=lambda x: x[1]['address'],
-                             reverse=False)
-      prev = None
-      prevName = None
+      sorted_names = sorted(sym_info.keys(),
+                            key=lambda name: sym_info[name]['address'],
+                            reverse=False)
+
       #assign padding based on difference in address
-      for sName, info in sortedSymbols:
-        if (prev != None):
-          self.sInfo[prevName][
-              'padding'] = info['address'] - prev['address'] - prev['size']
-        prev = info
-        prevName = sName
+      for i, name in enumerate(sorted_names):
+        if (i == 0):
+          continue
+        prev_sym = sym_info[sorted_names[i - 1]]
+        cur_sym = sym_info[name]
+        prev_sym['padding'] = cur_sym['address'] - prev_sym[
+            'address'] - prev_sym['size']
 
       #sort by size
-      sortedSymbols = sorted(sortedSymbols,
-                             key=lambda x: x[1]['size'],
-                             reverse=True)
+      sorted_names = sorted(sym_info.keys(),
+                            key=lambda name: sym_info[name]['size'],
+                            reverse=True)
+
       #print symbols in order of size with padding
-      for sName, info in sortedSymbols:
+      for name in sorted_names:
+        cur_sym = sym_info[name]
         print(
-            f"Name: {sName}, Address: {hex(info['address'])}, Size: {hex(info['size'])}, Padding: {hex(info['padding'])}"
+            f"Name: {name}, Address: {hex(cur_sym['address'])}, Size: {hex(cur_sym['size'])}, Padding: {hex(cur_sym['padding'])}"
         )
 
 
